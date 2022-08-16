85db4d2c9baed775734ffe8809fbe95b5f5d6cef
jhorman9 <79775604+jhorman9@users.noreply.github.com>
2022-08-15 21:37:46 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,29 +1,42 @@
-const display =document.querySelector(".display");//solo llama este solo.
-//console.log(display);//esto es solo para verlo en consola del navegador
-const buttons = document.querySelectorAll(".btn");//para obtener el nmr4
-let value = 0;
-/*console.log(buttons)
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll(".btn");
 
-function escuchando(e){
-    console.log(e)
+let value = "";
+let newValue = "";
+
+function calcular(e) {
+  const tecla = e.target.textContent;
+  switch (tecla) {
+    case "=":
+      display.textContent = eval(value.replace("x", "*"));
+      value = eval(value.replace("x", "*"));
+      break;
+    case "AC":
+      display.textContent = "0";
+      value = "";
+      break;
+    case "%":
+      let values = [];
+      //cuando presiono el porcentaje
+      //calcula el porcentaje del valor que puse
+      // se queda con la operacion
+      newValue = value.replace(/[^\d?]/g, " ");
+      const oeprador = value.replace(/[\d?]/g, " ").trim();
+      values = newValue.split(" ");
+      let porcentaje = (values[0] * values[1]) / 100;
+      value = `${values[0]}${oeprador}${porcentaje}`;
+      display.textContent = value;
+      break;
+    case "+":
+    case "-":
+    case "x":
+    case "/":
+    default:
+      value += tecla;
+      display.textContent = value;
+  }
 }
 
-buttons.addEventListener("click", escuchando)//se crea la function para que lo imprima
-
-function addEventListener(evento,funct){
-    //codigo
-    funct("dblclick")
-}*/
-
-function escuchando(e){
-    //console.log(e.target.textContent);//se pone el target, el textContent es para saber que texto me esta dando cada uno.
-    value += e.target.textContent;//concatena y asigna
-    display.textContent = value;//
-
-}
-
-for(let i= 0; i<buttons.length; i++){
-    //console.log(buttons[i])//para saber si me esta corriendo
-    
-    buttons[i].addEventListener("click", escuchando)//ver el target del console del navegador ya se agrego un evento a todos los botones de esta manera. Solo se agrego el evento.
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", calcular);
 }
